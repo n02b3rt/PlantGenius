4796a39c73b339f719266e09903665f973c0bf50
Norbert Fxx <93492117+n02b3rt@users.noreply.github.com>
2025-05-15 21:46:36 +0200

    [INIT]

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-200 to-blue-200">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Tailwind działa!
+      </h1>
+    </div>
+  );
+}
